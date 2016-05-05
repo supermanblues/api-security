2f94457c42970ede3069ce3be537b7f3152f9282
王强 <wangqiang02@wondersgroup.com>
2016-05-05 15:15:53 +0800

    Initial commit

---
+++ src/main/groovy/com/wonders/xlab/apisecurity/ApiSecurityApplication.groovy
@@ -0,0 +1,12 @@
+package com.wonders.xlab.apisecurity
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApiSecurityApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ApiSecurityApplication, args
+	}
+}
